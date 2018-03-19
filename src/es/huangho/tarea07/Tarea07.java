3a052b03339be0612652bf27c3d25982fa426d66
huangho2018 <37557619+huangho2018@users.noreply.github.com>
2018-03-19 22:47:07 +0100

    Add files via upload

---
+++ src/es/huangho/tarea07/Tarea07.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.huangho.tarea07;
+
+/**
+ *
+ * @author Juan Jose Gómez Vázquez <DAW 2015/2016. MP0485. Programación>
+ */
+public class Tarea07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario f=new Formulario();
+        f.setVisible(true);
+    }
+    
+}
